21bc25cc1755c38e67785d4a53c0de2aacc1aa96
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-12 15:47:43 +0530

    spark

---
+++ concate.java
@@ -0,0 +1,10 @@
+public class concate
+{
+    public static void main(String[] args)
+    {
+        String firstname="jaya";
+        String lastname="krishna";
+        String fullname=firstname+" "+lastname;
+        System.out.println("fullname:"+fullname);
+    }
+}      
