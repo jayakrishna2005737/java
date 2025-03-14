1d608aff04a4b15594ad429351e4e43a133bb927
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 11:58:01 +0530

    continue using loops

---
+++ continues.java
@@ -0,0 +1,15 @@
+public class continues
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i);
+            if(i==7)
+            {
+                continue;
+            }
+        }
+    }
+}
