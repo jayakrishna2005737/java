e1180504ec450c572fd4b301eda2c566eeb75e31
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 10:29:18 +0530

    vowels letters

---
+++ vowels.java
@@ -0,0 +1,16 @@
+public class vowels
+{
+    public static void main(String[] args)
+    {
+        char ch='R';
+        if(ch=='A'||ch=='E'||ch=='O'||ch=='U')
+        {
+            System.out.println("vowel");
+        }
+        else
+        {
+            System.out.println("not a vowel");
+        }   
+        
+    }
+}
