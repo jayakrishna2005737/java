c24a73a1fe9e6e69cd7762b75ea8eab18a499396
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 11:43:53 +0530

    forloops sum

---
+++ forloops.java
@@ -0,0 +1,23 @@
+public class forloops
+{
+    public static void main(String[] args)
+    {
+        String str="my name is jayakrishna";
+        int i;
+        for(i=0;i<10;i++)
+        {
+            System.out.println(str);
+        }
+        for(i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+        
+        {
+        for(i=20;i>10;i--)
+        {
+            System.out.println(i);
+        }    
+        }
+    }
+}            
