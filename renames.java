59cb01b9cad69728b0974bdd8698c4e9ad8c60a9
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 15:15:19 +0530

    renames

---
+++ renames.java
@@ -0,0 +1,10 @@
+public class renames
+{
+    public static void main(String[] args)
+    {
+        String[] fruits={"apple","mango"};
+        fruits[0]="banana";
+        for(int i=0;i<fruits.length;i++)
+        System.out.println(fruits[1]);
+    }
+}
