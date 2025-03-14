97518bda73ad0ce114ef0b1ac8f307bd7b68b21b
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 13:59:53 +0530

    arrays using variables

---
+++ arrays.java
@@ -0,0 +1,13 @@
+public class arrays
+{
+    public static void main(String[] args)
+    {
+        String[] fruits={"apple","mango","orange","pineapple","Strawberry"};
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+        System.out.println(fruits[4]);
+        System.out.println("length of the array:"+fruits.length);
+    }
+}
