8210c3db024f0106ec53069788b9e6580d836c50
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-14 14:26:33 +0530

    two dimensional array

---
+++ dimension.java
@@ -0,0 +1,8 @@
+public class dimension
+{
+    public static void main(String[] args)
+    {
+       String[][] game={{"cricket","ludo"},{"kabaddi","football"}};
+       System.out.println(game[1][1]);
+    }
+}
