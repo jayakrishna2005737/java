cdf6ca4ff32caafb845f0a6bb8ab704a731db5bf
jayakrishna2005737 <jayakrishnak48@gmail.com>
2025-03-15 12:16:06 +0530

    Scanner using finding

---
+++ scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class scanner
+{
+    public static void main(String[] args)
+    {
+        Scanner myobj=new Scanner(System.in);
+        System.out.println("Enter username");
+        String username=myobj.next();
+        System.out.println("the username is"+username);
+    }
+}
